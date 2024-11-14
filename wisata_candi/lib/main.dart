24da14c5f4e22d07ff61933795fae97d4f02ba4d
serenitydevina <147176974+serenitydevina@users.noreply.github.com>
2024-11-14 13:08:11 +0700

    13 November 2024 (navigasi dan routing)

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_candi/screens/favorite_screen.dart';
 import 'package:wisata_candi/screens/home_screen.dart';
-// import 'package:wisata_candi/screens/search_screen.dart';
-// import 'package:wisata_candi/data/candi_data.dart';
-// import 'package:wisata_candi/screens/profile_screen.dart';
-// import 'package:wisata_candi/screens/sign_in_screen.dart';
-// import 'package:wisata_candi/screens/sign_up_screen.dart';
-// import 'screens/detail_screen.dart';
-// import 'screens/profile_screen.dart';
+import 'package:wisata_candi/screens/search_screen.dart';
+import 'package:wisata_candi/screens/home_screen.dart';
+import 'package:wisata_candi/screens/search_screen.dart';
+import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/profile_screen.dart';
+import 'package:wisata_candi/screens/sign_in_screen.dart';
+import 'package:wisata_candi/screens/sign_up_screen.dart';
+import 'screens/detail_screen.dart';
+import 'screens/profile_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -42,7 +45,68 @@ class MainApp extends StatelessWidget {
       // SignInScreen(),
       // SignUpScreen(),
       // SearchScreen(),
+      // HomeScreen(),
+      MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+    //TODO: 1. Deklarasikan variabel
+    int _currentIndex = 0;
+    final List<Widget> _children = [
       HomeScreen(),
+      SearchScreen(),
+      FavoriteScreen(),
+      ProfileScreen(),
+    ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //TODO: 2. Buat properti body berupa widget yang ditampilkan
+      body: _children[_currentIndex],
+      //TODO: 3. Buat properti bottomNavigationBar dengan nilai Theme
+      bottomNavigationBar: Theme(
+        //TODO: 4. Buat data dan child dari Theme
+        data: Theme.of(context).copyWith(canvasColor: Colors.deepPurple[50],),
+        child:BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index){
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home,color: Colors.deepPurple,
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search,color: Colors.deepPurple,),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite,color: Colors.deepPurple,),
+              label: 'Favorite',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person,color: Colors.deepPurple,),
+              label: 'Profile',
+            ),
+          ],
+          selectedItemColor: Colors.deepPurple,
+          unselectedItemColor: Colors.deepPurple[100],
+          showSelectedLabels: true,
+          ) ,
+        ),
     );
   }
 }
